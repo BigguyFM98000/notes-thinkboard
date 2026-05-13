a845cecb70004bd0db9b65b12128aed0505db2e0
Fhatuwani Raphalalani <fm.raphalalani29@gmail.com>
2026-05-13 15:47:30 +0200

    finished with crud

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -43,10 +43,12 @@ const HomePage = () => {
             <div className="max-w-7xl mx-auto p-4 mt-6">
                 {isLoading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
+                {notes.length === 0 && !RateLimitedUI && <NotesNotFound />}
+
                 {notes.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {notes.map((note) => (
-                            <NoteCard key={note._id} note={note} />
+                            <NoteCard key={note._id} note={note} setNotes={setNotes}/>
                         ))}
                     </div>
                 )}
