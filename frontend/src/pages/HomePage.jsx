e303f29890c5f18ac270b910dcffec28f41f1457
Fhatuwani Raphalalani <fm.raphalalani29@gmail.com>
2026-05-13 13:14:54 +0200

    added create note and home pages

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import toast from "react-hot-toast";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import RateLimitedUI from "../components/RateLimitedUI.jsx";
 import Navbar from "../components/Navbar.jsx";
 import NoteCard from "../components/NoteCard.jsx";
+import api from "../lib/axios.js";
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(false);
@@ -13,7 +13,7 @@ const HomePage = () => {
     useEffect(() => {
         const fetchNotes = async () => {
             try {
-                const res = await axios.get("http://localhost:5050/api/notes");
+                const res = await api.get("/notes");
     
                 console.log(res.data);
                 setNotes(res.data);
